b3cf67e20d7eb5613abb0f296ddaf0aecef62ae8
kuba <jan.kowalski@example.com>
2025-05-31 11:27:32 +0200

    Commit 1

--- UserInterface/options_view.py
+++ UserInterface/options_view.py
@@ -1,31 +1,89 @@
 import arcade
-
-from arcade.gui import UIFlatButton, UIManager, UIAnchorLayout, UILabel, UIBoxLayout, UIInputText, UIGridLayout
-from docutils.parsers.rst.directives.misc import Title
-
-from Database.models import User
-from Database.session import Session
-from UserInterface import main_menu
+from arcade.gui import UIManager, UIAnchorLayout, UIBoxLayout, UILabel, UISlider
 from UserInterface.view_utils import BaseView
 
 
 class Options(BaseView):
     def __init__(self):
         super().__init__()
-        self.back_view=self.window.main_view
-        self.anchor = self.manager.add(UIAnchorLayout())
+        self.manager = UIManager()
+        self.manager.enable()
+
+        # Anchor layout do pozycjonowania
+        self.anchor_layout = UIAnchorLayout()
+        self.manager.add(self.anchor_layout)
+
+        # Tytuł
         title_box = UIBoxLayout(vertical=True, space_between=20)
-        main_label = UILabel(text="Ustawienia", width=1000, text_color=arcade.color.BLACK, font_size=100,
-                             font_name="Arial")
+        main_label = UILabel(
+            text="Ustawienia",
+            font_size=60,
+            text_color=arcade.color.BLACK,
+            width=600,
+            font_name="Arial"
+        )
         title_box.add(main_label)
-        self.anchor.add(
+        self.anchor_layout.add(
             child=title_box,
             anchor_x="center",
             anchor_y="top",
             align_y=-30
         )
 
+        # Kontener na głośność
+        volume_box = UIBoxLayout(vertical=True, space_between=20)
 
+        # Label pokazujący głośność
+        self.volume_label = UILabel(
+            text=f"Głośność: {int(self.window.volume * 100)}%",
+            font_size=30,
+            text_color=arcade.color.BLACK
+        )
+        volume_box.add(self.volume_label)
 
+        # Suwak do regulacji głośności
+        self.volume_slider = UISlider(
+            value=self.window.volume,
+            min_value=0.0,
+            max_value=1.0,
+            width=300,
+            step=0.01
+        )
+        self.volume_slider.on_change = self.on_volume_change
+        volume_box.add(self.volume_slider)
 
+        self.anchor_layout.add(
+            child=volume_box,
+            anchor_x="center",
+            anchor_y="center",
+            align_y=-50
+        )
 
+    def on_volume_change(self, event):
+        new_volume = self.volume_slider.value
+        self.window.volume = new_volume
+        self.update_volume_label()
+        self.set_music_volume(new_volume)
+
+    def update_volume_label(self):
+        self.volume_label.text = f"Głośność: {int(self.window.volume * 100)}%"
+
+    def set_music_volume(self, volume):
+        if self.window.music_player:
+            try:
+                # Zmiana głośnośći "na żwo"
+                self.window.music_player.volume = volume
+            except AttributeError:
+                # Jak nie pójdzie, to restartujemy muzykę z nową głośnością
+                self.window.music_player.pause()
+                self.window.music_player = self.window.background_music.play(volume=volume, loop=True)
+
+    def on_show_view(self):
+        self.manager.enable()
+
+    def on_hide_view(self):
+        self.manager.disable()
+
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from UserInterface.scoreboard_view import ScoreboardView
 class HangmanGame(arcade.Window):
     def __init__(self):
         super().__init__(1280, 720, "Hangman")
+        self.background_music = arcade.load_sound("Sounds/MuzykaGra.mp3")
+        self.volume = 0.1
+        self.music_player = self.background_music.play(self.volume, loop=True)
         self.login_view = LoginView()
         self.main_view = MainMenu()
         self.register_view=RegisterView()
@@ -17,6 +20,12 @@ class HangmanGame(arcade.Window):
         self.info_view = InfoView()
         self.scoreboard_view = ScoreboardView()
         self.show_view(self.login_view)
+
+        def on_close(self):
+            # Zatrzymaj muzykę przy zamknięciu gry
+            if self.music_player:
+                self.music_player.pause()
+            super().on_close()
 
 
 
